d86842b9c2ea3f22d00548d9e71ddf063efa5254
Dallascampbell <dallasbcampbell@gmail.com>
2023-02-21 13:43:57 -0700

    Add project files.

---
+++ Mission08_Group2-11/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission08_Group2_11.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
