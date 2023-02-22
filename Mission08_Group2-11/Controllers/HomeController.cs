cb224123c4c631ce36c44b58ab94bb463db51b5a
wirickad <92128478+wirickad@users.noreply.github.com>
2023-02-22 12:45:04 -0700

    added quadrant

--- Mission08_Group2-11/Controllers/HomeController.cs
+++ Mission08_Group2-11/Controllers/HomeController.cs
@@ -23,13 +23,55 @@ namespace Mission08_Group2_11.Controllers
             return View();
         }
 
-        public IActionResult AddEdit()
+        [HttpGet]
+        public IActionResult AddTask()
         {
-            return View();
+            return View("AddEdit");
         }
+
+        /*[HttpPost]
+        public IActionResult AddTask()
+        {
+            return View("AddEdit");
+        }*/
+
+        /*[HttpGet]
+        public IActionResult Edit(int TaskId)
+        {
+            ViewBag.Categories = movieContext.Categories.ToList();
+            var movieForm = movieContext.Movies.Single(x => x.MovieId == MovieId);
+            return View("AddMovie", movieForm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie mov)
+        {
+            movieContext.Update(mov);
+            movieContext.SaveChanges();
+            return RedirectToAction("MovieList");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int MovieId)
+        {
+            var movie = movieContext.Movies.Single(x => x.MovieId == MovieId);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Movie mov)
+        {
+            movieContext.Movies.Remove(mov);
+            movieContext.SaveChanges();
+            return RedirectToAction("MovieList");
+        }*/
 
         public IActionResult Quadrant()
         {
+            /*var q1 = movieContext.Responses.Include(x => x.Category).ToList();
+            var q2 = movieContext.Responses.Include(x => x.Category).ToList();
+            var q3 = movieContext.Responses.Include(x => x.Category).ToList();
+            var q4 = movieContext.Responses.Include(x => x.Category).ToList();*/
             return View();
         }
 
